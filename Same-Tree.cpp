c54bb5f764ea7956b11a009aafe954d35b10b93e
RajdeepSingh parmar <rajdeepsinghparmar.142893@marwadiuniversity.ac.in>
2026-08-10 23:26:57 +0530

    Create Same-Tree.cpp

---
+++ Same-Tree.cpp
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/same-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        // Base case: both nodes are null
+        if (!p && !q) return true;
+        
+        // One is null, the other isn't
+        if (!p || !q) return false;
+        
+        // Values don't match
+        if (p->val != q->val) return false;
+        
+        // Recursively check left and right subtrees
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
